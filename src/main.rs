f1baee462c30815932066986f27d22c01b36828e
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-07-16 20:56:38 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,15 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
+    // Your Program should:
+    // 1. ✅ Display the prompt $
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    // 2. Read the user's input
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    println!("{}: command not found", input.trim());
+    // 3. Print an error message in exactly this format: {command}: command not found
+    //  ๏ e.g. if the user types xyz, print xyz: command not found
 }
