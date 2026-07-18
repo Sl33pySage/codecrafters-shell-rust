a8c3a5168978a57f3a08f4632056c8b5e9a12f56
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-07-18 01:56:34 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Your Program should:
-    // 1. ✅ Display the prompt $
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        // Your Program should:
+        // 1. ✅ Display the prompt $
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // 2. Read the user's input
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    println!("{}: command not found", input.trim());
-    // 3. Print an error message in exactly this format: {command}: command not found
-    //  ๏ e.g. if the user types xyz, print xyz: command not found
+        // 2. Read the user's input
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        // 3. Print an error message in exactly this format: {command}: command not found
+        //  ๏ e.g. if the user types xyz, print xyz: command not found
+        println!("{}: command not found", input.trim());
+    }
 }
