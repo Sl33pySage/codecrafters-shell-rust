3a6aa68619f2424c92afcedad2fff29ee3a5ea3d
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-07-18 15:53:10 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,18 @@ use std::io::{self, Write};
 
 fn main() {
     loop {
-        // Your Program should:
-        // 1. ✅ Display the prompt $
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        // 2. Read the user's input
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        if input.trim() == "exit" {
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+        command = command.trim().to_string();
+        if command == "exit" {
             break;
+        } else if command.starts_with("echo") {
+            println!("{}", &command[5..]);
         } else {
-            // 3. Print an error message in exactly this format: {command}: command not found
-            //  ๏ e.g. if the user types xyz, print xyz: command not found
-            println!("{}: command not found", input.trim());
+            println!("{}: command not found", command.trim());
         }
     }
 }
