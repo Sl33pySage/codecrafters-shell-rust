d5c827f07b6ac26eb4dae2c3d80d4cbb33f6a310
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-07-18 02:16:11 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,10 @@ fn main() {
         io::stdin().read_line(&mut input).unwrap();
         if &mut input == "exit" {
             break;
+        } else {
+            // 3. Print an error message in exactly this format: {command}: command not found
+            //  ๏ e.g. if the user types xyz, print xyz: command not found
+            println!("{}: command not found", input.trim());
         }
-        // 3. Print an error message in exactly this format: {command}: command not found
-        //  ๏ e.g. if the user types xyz, print xyz: command not found
-        println!("{}: command not found", input.trim());
     }
 }
