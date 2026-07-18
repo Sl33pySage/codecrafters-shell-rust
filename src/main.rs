2a48511debd7da28698ac7a4547c57461c7bd4c1
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-07-18 02:10:41 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,9 @@ fn main() {
         // 2. Read the user's input
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
+        if &mut input == "exit" {
+            break;
+        }
         // 3. Print an error message in exactly this format: {command}: command not found
         //  ๏ e.g. if the user types xyz, print xyz: command not found
         println!("{}: command not found", input.trim());
