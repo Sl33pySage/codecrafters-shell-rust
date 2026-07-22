336e89a4757b3fd6274bc5cafd118d27e97cec22
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-07-22 08:43:29 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,22 @@ use std::io::{self, Write};
 use std::os::unix::fs::PermissionsExt;
 use std::path::PathBuf;
 
-fn plswork(path: PathBuf) -> std::io::Result<()> {
+fn plswork(path: PathBuf, command: String) -> std::io::Result<()> {
     let metadata = fs::metadata(&path)?;
-    println!("{:o}", !metadata.permissions().mode());
+    // Retrieve the raw bits (u32)
+    let permissions = metadata.permissions();
+    let mode = permissions.mode();
+    // Print the octal format (Standard unix representation like 755)
+    //println!("{:o}", mode);
+    // Check if the lower 9 bits contain specefic permissions (e.g., owner execute)
+    let is_owner_executable = (mode & 0o100) != 0;
+    if is_owner_executable {
+        //  TODO: c. If the file exists and has execute permissions, print <command> is
+        //  <full_path> and stop.
+        println!("{} is {}", command, path.display());
+    } else if !is_owner_executable {
+        //  TODO: d. If the file exists but lacks execute permissions, skip it and continue to the next directory.
+    }
     Ok(())
 }
 
@@ -24,9 +37,6 @@ fn main() {
         } else if command.starts_with("echo") {
             println!("{}", &command[5..]);
         } else if command.starts_with("type") {
-            //  TODO: c. If the file exists and has execute permissions, print <command> is
-            //  <full_path> and stop.
-            //  TODO: d. If the file exists but lacks execute permissions, skip it and continue to the next directory.
             // TODO: 3. If no executable is found in any directory, print <command> : not found.
             if &command[5..] == "echo" || &command[5..] == "type" || &command[5..] == "exit" {
                 println!("{} is a shell builtin", &command[5..]);
@@ -36,17 +46,12 @@ fn main() {
                 match env::var_os(key) {
                     Some(paths) => {
                         for path in env::split_paths(&paths) {
-                            println!("'{}'", path.display());
+                            //println!("'{}'", path.display());
                             for file in &path {
                                 //  TODO: a. Check if a file with the command name exists.
                                 if file == &command[5..] {
-                                    //checking for permissions
-                                    // println!("MATCH: {:?}", file);
                                     //  TODO: b. Check if the file has execute permissions.
-
-                                    //let permissions = metadata.permissions();
-                                    //println!("{:?}", permissions);
-                                    plswork(path.clone());
+                                    plswork(path.clone(), command[5..].to_string());
                                 }
                             }
                         }
